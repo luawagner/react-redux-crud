d5069829d00dfab21c17ed8d0c98edc7fad51449
ludmila wagner <lu.wagner91@gmail.com>
2023-10-25 11:06:08 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import ListOfUsers from './components/ListOfUsers'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1>Crud con redux</h1>
+     <ListOfUsers />
+    </>
+  )
+}
+
+export default App
